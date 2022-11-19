476524404d3004b9538dfe5199a324ac9b845dbc
Filip Durca <filip.durca@gmail.com>
2022-11-19 17:55:22 -0500

    Rebasing

--- Backend/noteTranslation.py
+++ Backend/noteTranslation.py
@@ -1,9 +1,11 @@
 # This class is used to summarize the notes of each slide
 # Imports
+import requests
 import speech_recognition as sr
 import cohere
 import time
 import pandas as pd
+from punctuator import Punctuator
 
 # Global variables
 api_key = "RG7HNjYkf3BPA7KkdWrb6VdqvVZXWNA78mNTwyvN"
@@ -20,7 +22,7 @@ def speechToText(audio):
 
     # Open the file and convert all recognized speech
     with sr.AudioFile(filename) as source:
-        audioData = r.record(source)
+        audioData = r.listen(source, 100, 100)
         return r.recognize_google(audioData)
 
 
@@ -28,6 +30,10 @@ def speechToText(audio):
 def summary(audio):
     # Translate the audio to text
     text = speechToText(audio)
+
+    # Punctuate data
+    punc = Punctuator('model.pcl')
+    text = punc.punctuate(text)
 
     # Leverage cohere to generate summary
     sumText = co.generate(
@@ -37,4 +43,6 @@ def summary(audio):
     )
 
     # Return summary
-    return sumText
+    return sumText[0]
+
+print(summary("test.wav"))
